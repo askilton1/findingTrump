fd12b6c5de47c0cb1aa2ecf73a0ba4692db2f61e
askilton1 <antonio.skilton@gmail.com>
2016-11-12 12:01:24 -0800

    more directory script; will only write tables if they do not exist and if csv is available

--- setup.R
+++ setup.R
@@ -1,20 +1,40 @@
+#place /programs in same directory along with a /data folder 
 #for a dplyr sqlite tutorial: https://cran.r-project.org/web/packages/dplyr/vignettes/databases.html
-#to download required data files:
+#to download required data files: https://www.dropbox.com/sh/ojjasinnk17mi0m/AAAJtygVHup26jDH9Y8_bTvha?dl=0
+library(tidyverse); library(RSQLite)
+#is this code being run on macOS? this will automate directory when Tristan runs code
+macOS <- grepl("apple", sessionInfo()$platform)
 
-library(tidyverse)
-my_db <- src_sqlite("finding_trump.db", create = T)
+#reset directory to folder containing folder containing .Rproj file
+original_wd <- getwd()
+if(macOS){new_wd <- gsub("/programs", "", getwd())} #else {new_wd <- gsub("\programs", "", getwd())}
+setwd(new_wd)
+
+#if database does not exist, create it
+create <- !("finding_trump.db" %in% list.files())
+my_db <- src_sqlite("finding_trump.db", create = create)
+tables <- src_tbls(my_db)
+
+#reset directory to data folder
+if(macOS){new_wd <- paste0(new_wd, "/data")} #else {new_wd <- paste0(new_wd, "\data")}
+setwd(new_wd)
 
 #2001
-read_csv("data/usa_00002.csv",
-         #do not read 2015 rows
-         n_max = 1192206) %>%
-  #select columns where values are not 100% missing
-  select_if(function(col) sum(is.na(col))/1192206 != 1) %>%
-  #create new table in database
-  copy_to(dest = my_db, df =  ., name = "ACS_2001", temporary = F)
+#if the csv is available and the ACS_2001 table does not exist
+if("usa_00002.csv" %in% list.files() & !("ACS_2001" %in% tables)){
+  read_csv("usa_00002.csv",
+           #do not read 2015 rows
+           n_max = 1192206) %>%
+    #select columns where values are not 100% missing
+    select_if(function(col) sum(is.na(col))/1192206 != 1) %>%
+    #create new table in database
+    copy_to(dest = my_db, df =  ., name = "ACS_2001", temporary = F)
+}
 
 #2015
-read_csv("data/usa_00002.csv",
+#if the csv is available and the ACS_2015 table does not exist
+if("usa_00002.csv" %in% list.files() & !("ACS_2015" %in% tables)){
+read_csv("usa_00002.csv",
          #skip 2001 rows
          skip = 1192207,
          #can't figure out how to both skip rows and retain header
@@ -23,4 +43,7 @@ read_csv("data/usa_00002.csv",
   select_if(function(col) sum(is.na(col))/1192206 != 1) %>%
   #create a new table in database
   copy_to(dest = my_db, df = ., name = "ACS_2015", temporary = F)
+}
 
+#reset working directory in case you want to run program again
+setwd(original_wd)
