c0f7ad0f1c790a1be27162798336d54f4d7f2a82
askilton1 <antonio.skilton@gmail.com>
2016-11-19 21:44:29 -0800

    added vars, changed some var names

--- county_data_tristan_in_hawaii_crappy_laptop.R
+++ county_data_tristan_in_hawaii_crappy_laptop.R
@@ -1,4 +1,6 @@
 temp %>%
   group_by(REGION, STATEFIP, COUNTY) %>%
-  summarise_if(is.numeric, mean) %>%
+  mutate(college = ifelse(HHEDUC == "Bachelors Degree" | HHEDUC == "Postgraduate study", 1, 0),
+         METRO = ifelse(METRO == "In metro area", 1, 0)) %>%
+  summarise(percent_white = mean(RACWHT), average_age = mean(AGE), median_hhincome = median(HHINCOME), percent_college_degree = mean(college), county_pop = n()) %>%
   save(.,file =  "while_in_hawaii.RData")
