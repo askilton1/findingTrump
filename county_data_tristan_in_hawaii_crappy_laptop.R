00a3ccfff02f86e2dbc94c3564b49fb3a60c1925
askilton1 <antonio.skilton@gmail.com>
2016-11-22 21:08:27 -0800

    everything adjusted for SQL Server

--- county_data_tristan_in_hawaii_crappy_laptop.R
+++
@@ -1,6 +0,0 @@
-temp %>%
-  group_by(REGION, STATEFIP, COUNTY) %>%
-  mutate(college = ifelse(HHEDUC == "Bachelors Degree" | HHEDUC == "Postgraduate study", 1, 0),
-         METRO = ifelse(METRO == "In metro area", 1, 0)) %>%
-  summarise(percent_white = mean(RACWHT), average_age = mean(AGE), median_hhincome = median(HHINCOME), percent_college_degree = mean(college), county_pop = n()) %>%
-  save(.,file =  "while_in_hawaii.RData")
