d61104b079239d8bb46a1ce664aa2d126ac1d5ff
askilton1 <antonio.skilton@gmail.com>
2016-11-25 13:51:21 -0800

    number of adults in labor force, number adult men not in labor force

--- households_view.sql
+++ households_view.sql
@@ -8,7 +8,9 @@ alter view household_level_variables as
 				max(college) [hh_College_Degree], --at least one person has college degree
 				max(hispanic) [hh_Hispanic], --at least one person in household is hispanic
 				max(female_head) [hh_Female_Head_of_Household], --"head of household" is female
-				max(PERNUM) [people_in_household]
+				max(PERNUM) [hh_size],
+				sum(in_labor_force) [hh_labor_force],
+				sum(adult_men_not_in_labor_force) [hh_adult_men_not_in_labor_force]
 			 from census_2013_5year_clean
 			 group by SERIAL) b
 			 on a.SERIAL = b.SERIAL
