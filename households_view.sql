51c02fea2680d01303b7ac2b707ed856591f576b
askilton1 <antonio.skilton@gmail.com>
2016-11-25 13:35:47 -0800

    household_level_variables now individual level
    and adds household level variables

--- households_view.sql
+++ households_view.sql
@@ -1,12 +1,13 @@
-create view households as
-	select SERIAL,
-				  STATEFIP, 
-				  COUNTY, 
-				  REGION, 
-				  METRO, 
-				  max(white) [White], --at least one person in household is white
-				  avg(HHINCOME) [Household income], 
-				  max(college) [College Degree], --at least one person has college degree
-				  max(hispanic) [Hispanic] --at least one person in household is hispanic
-		from census20_clean
-		group by REGION, METRO, STATEFIP, COUNTY, SERIAL
+alter view household_level_variables as
+	select a.*, b.hh_White, b.hh_College_Degree, b.hh_Hispanic, b.hh_Female_Head_of_Household
+		from census_2013_5year_clean a
+		inner join
+			(select 
+				SERIAL,
+				max(white) [hh_White], --at least one person in household is white
+				max(college) [hh_College_Degree], --at least one person has college degree
+				max(hispanic) [hh_Hispanic], --at least one person in household is hispanic
+				max(female_head) [hh_Female_Head_of_Household]
+			 from census_2013_5year_clean
+			 group by SERIAL) b
+			 on a.SERIAL = b.SERIAL
