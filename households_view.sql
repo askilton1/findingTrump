53eee33a7b525aa87f73cf92426417bff7a75587
askilton1 <antonio.skilton@gmail.com>
2016-11-25 13:41:12 -0800

    added number of people in household, specifying ungrouped variables is easier

--- households_view.sql
+++ households_view.sql
@@ -1,5 +1,5 @@
 alter view household_level_variables as
-	select a.*, b.hh_White, b.hh_College_Degree, b.hh_Hispanic, b.hh_Female_Head_of_Household
+	select a.REGION, a.STATEFIP, a.COUNTY, a.METRO, a.HHINCOME, a.AGE, a.RACE, b.*
 		from census_2013_5year_clean a
 		inner join
 			(select 
@@ -7,7 +7,8 @@ alter view household_level_variables as
 				max(white) [hh_White], --at least one person in household is white
 				max(college) [hh_College_Degree], --at least one person has college degree
 				max(hispanic) [hh_Hispanic], --at least one person in household is hispanic
-				max(female_head) [hh_Female_Head_of_Household]
+				max(female_head) [hh_Female_Head_of_Household], --"head of household" is female
+				max(PERNUM) [people_in_household]
 			 from census_2013_5year_clean
 			 group by SERIAL) b
 			 on a.SERIAL = b.SERIAL
