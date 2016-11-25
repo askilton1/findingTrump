e2984689e4d3e8eec4ecb64ef11ba3b3d3a3c360
askilton1 <antonio.skilton@gmail.com>
2016-11-25 13:35:06 -0800

    new view name doesn't have to change when vars added, include RELATE

--- SQL_cleaning/census20_clean_view.sql
+++ SQL_cleaning/census20_clean_view.sql
@@ -1,27 +1,31 @@
-alter view census20_clean
+alter view census_2013_5year_clean
 as
-	select RECTYPE, DATANUM, SERIAL, PERNUM, REGION, STATEFIP, COUNTY, METRO, HHINCOME, AGE, RACE,
+	select RECTYPE, DATANUM, SERIAL, RELATE, PERNUM, REGION, STATEFIP, COUNTY, METRO, HHINCOME, AGE, RACE,
 			case 
 				when HISPAN > 0 then 1
 				else 0
-			end 
-				as hispanic,
+			end  as hispanic,
 			case
 				when EDUC = 11 then 5 -- 5 of more years of college
 				when EDUC = 10 then 4 -- four years of college
 				when EDUC <= 9 then 3 -- some college
 				when EDUC = 6 then 2 -- high school degree
 				when EDUC <= 5 then 1 -- less than high school degree
-			end 
-				as educ,
+			end as educ,
+			case 
+				when EDUC >= 6 then 1
+				else 0
+			end as college,
 			case 
 				when SEX = 2 then 0
 				else 1
-			end
-				as male,
+			end as male,
 			case
-				when RACE = 1 then 0
-				else 1
-			end
-				as white
-	from census20
+				when RACE = 1 then 1
+				else 0
+			end as white,
+			case
+				when SEX = 2 and RELATE = 1 then 1
+				else 0
+			end as female_head
+	from census_2013_5year
