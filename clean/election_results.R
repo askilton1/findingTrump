4276ef46899699e70ab6509ab5dedb624d8f2acf
askilton1 <antonio.skilton@gmail.com>
2016-11-28 12:44:19 -0800

    filter out non county rows

--- clean/election_results.R
+++ clean/election_results.R
@@ -4,13 +4,17 @@ substrRight <- function(x, n){
   substr(x, nchar(x)-n+1, nchar(x))
 }
 
-read_csv("data/raw/Pres_Election_Data_2016i.csv", col_types = cols_only(X1 = col_character(),
-                                                                        FIPS = col_character(),
-                                                                        ST = col_integer(),
-                                                                        X2 = col_character(),
-                                                                        Clinton = col_character(),
-                                                                        Trump = col_character()),
-         na = "-") %>% 
+read_csv("data/raw/Pres_Election_Data_2016i.csv", 
+         na = "-", 
+         col_types = cols_only(X1 = col_character(),
+                               FIPS = col_character(),
+                               ST = col_integer(),
+                               X2 = col_character(),
+                               Clinton = col_character(),
+                               Trump = col_character(),
+                               LSAD_TRANS = col_character())) %>% 
+  filter(LSAD_TRANS != "State") %>% 
+  select(-LSAD_TRANS) %>% 
   slice(-1) %>% 
   rename(county_name = X1, 
          STATEABB = X2,
@@ -18,5 +22,5 @@ read_csv("data/raw/Pres_Election_Data_2016i.csv", col_types = cols_only(X1 = col
          STATEFIPS = ST) %>% 
   mutate_at(3:4, funs(as.numeric(gsub("%", "", .))/100)) %>% 
   na.omit %>% 
-  mutate(COUNTY = as.numeric(substrRight(COUNTY, 3))) 
+  mutate(COUNTY = as.numeric(substrRight(COUNTY, 3))) %>% 
   write_csv("data/clean/Pres_Election_DatA_2016i.csv", na = "")
