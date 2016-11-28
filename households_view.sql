4296341e1c3beeebb8044c19460d680e19d8766d
askilton1 <antonio.skilton@gmail.com>
2016-11-28 12:43:23 -0800

    create view

--- households_view.sql
+++ households_view.sql
@@ -1,9 +1,9 @@
-alter view census_hhold as
+create view census_hhold as
 	select a.REGION, a.STATEFIP, a.COUNTY, a.METRO, a.HHINCOME, a.AGE, a.RACE, b.*
 		from census_2013_5year_clean a
 		inner join
 			(select 
-				SERIAL,
+			    SERIAL,
 				max(white) [hh_White], --at least one person in household is white
 				max(high_school_degree) [hh_High_School_Degree], --at least one person has college degree
 				max(college) [hh_College_Degree], --at least one person has college degree
