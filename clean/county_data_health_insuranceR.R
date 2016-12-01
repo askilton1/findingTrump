df91757b132c75bce372fa42414a56d48310a723
askilton1 <antonio.skilton@gmail.com>
2016-12-01 13:40:27 -0800

    fixed horrendous variable name

--- clean/county_data_health_insuranceR.R
+++ clean/county_data_health_insuranceR.R
@@ -19,4 +19,5 @@ read_csv("data/raw/County_Data_2016_health_insurance.csv",
             `2013_uninsured_rate`,
             `2016_uninsured_rate`,
             decrease_from_2013_to_2016) %>% 
+  rename(change = decrease_from_2013_to_2016) %>% 
   write_csv("data/clean/County_Data_2016_health_insurance.csv")
