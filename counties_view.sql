8f2db1bb8f8eb8d9e2e6fe0bcfcd2b4694dc4025
askilton1 <antonio.skilton@gmail.com>
2016-12-01 13:33:06 -0800

    code now successfully creates two detailed variables
    but is much slower than PIVOT

--- counties_view.sql
+++ counties_view.sql
@@ -1,25 +1,21 @@
-alter view census_counties as
-select a.*, d.white_percent_in_college_hhold, e.nonwhite_percent_in_college_hhold
-from election_data a
-inner join 
-	(select c.STATEFIP, c.COUNTY, avg(c.white_hh_College_Degree) [white_percent_in_college_hhold]
+--alter view census_counties as
+select d.STATEFIP, d.COUNTY, d.white_percent_in_college_hhold, e.nonwhite_percent_in_college_hhold
+from
+	(select c.STATEFIP, c.COUNTY, avg(c.hh_College_Degree) [white_percent_in_college_hhold]
 	from
-		(select a.STATEFIP, a.COUNTY, RACE, cast(b.hh_College_Degree as float) [white_hh_College_Degree]
-		from census_2013_5year_clean a
+		(select a.STATEFIP, a.COUNTY, a.RACE, cast(b.hh_College_Degree as float) [hh_College_Degree]
+		from census_clean a
 		left outer join census_hhold b
 		on a.SERIAL = b.SERIAL) c
 	where RACE = 1
 	group by c.STATEFIP, c.COUNTY) d
-on a.STATEFIPS = d.STATEFIP and a.COUNTY = d.COUNTY
 inner join 
-	(select c.STATEFIP, c.COUNTY, avg(c.nonwhite_hh_College_Degree) [nonwhite_percent_in_college_hhold]
+	(select c.STATEFIP, c.COUNTY, avg(c.hh_College_Degree) [nonwhite_percent_in_college_hhold]
 	from
-		(select a.STATEFIP, a.COUNTY, RACE, cast(b.hh_College_Degree as float) [nonwhite_hh_College_Degree]
-		from census_2013_5year_clean a
+		(select a.STATEFIP, a.COUNTY, a.RACE, cast(b.hh_College_Degree as float) [hh_College_Degree]
+		from census_clean a
 		left outer join census_hhold b
 		on a.SERIAL = b.SERIAL) c
 	where RACE != 1
 	group by c.STATEFIP, c.COUNTY) e
-on e.STATEFIP = d.STATEFIP and e.COUNTY = d.COUNTY
-
-select * from census_counties
+on d.STATEFIP = e.STATEFIP and d.COUNTY = e.COUNTY
