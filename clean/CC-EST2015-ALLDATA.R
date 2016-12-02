26c4a3374bb219e866759ac1402008355e1b9ee4
askilton1 <antonio.skilton@gmail.com>
2016-12-01 17:56:44 -0800

    refined unemployment data, demographic data, added to notebook

--- clean/CC-EST2015-ALLDATA.R
+++ clean/CC-EST2015-ALLDATA.R
@@ -1,14 +1,16 @@
 library(tidyverse)
-
-### RANDOM FOREST ----
+#https://www.census.gov/popest/data/counties/asrh/2015/CC-EST2015-ALLDATA.html
 read_csv("data/raw/CC-EST2015-ALLDATA.csv",
-         col_types = list(STATE = col_integer(),
-                          COUNTY = col_integer())) %>% 
+         col_types = cols_only(STATE = col_integer(),
+                               COUNTY = col_integer(),
+                               TOT_POP = col_integer(),
+                               AGEGRP = col_integer(),
+                               YEAR = col_integer(),
+                               NHWA_MALE = col_integer(),
+                               NHWA_FEMALE = col_integer())) %>% 
   filter(AGEGRP >= 5,
          YEAR == 8) %>% 
-  rename(STATEFIPS = STATE) %>% 
-  group_by(STATEFIPS, COUNTY, AGEGRP) %>% 
-  summarise_at(vars(TOT_POP:HNAC_FEMALE), sum) %>% 
-  ungroup %>% 
-  mutate_at(vars(TOT_MALE:HNAC_FEMALE), funs(. / TOT_POP)) %>% 
+  rename(STATEFIPS = STATE) %>%
+  group_by(STATEFIPS, COUNTY) %>% 
+  summarise(NHWA = sum(NHWA_MALE + NHWA_FEMALE) / sum(TOT_POP)) %>% 
   write_csv("data/clean/CC-EST2015-ALLDATA.csv")
--- clean/Unemployment.R
+++ clean/Unemployment.R
@@ -3,9 +3,9 @@ library(tidyverse)
 read_csv("data/raw/Unemployment.csv", skip = 6, 
          col_types = cols_only(FIPS_Code = col_character(),
                                Unemployment_rate_2015 = col_double(),
-                               Civilian_labor_force_2015 = col_number(),
                                Median_Household_Income_2014 = col_number())) %>% 
-  mutate(COUNTY = as.numeric(substr(FIPS_Code, 3, 5)),
-         STATEFIPS = as.numeric(substr(FIPS_Code, 1, 2))) %>% 
-  select(-FIPS_Code) %>% 
+  transmute(STATEFIPS = as.numeric(substr(FIPS_Code, 1, 2)),
+            COUNTY = as.numeric(substr(FIPS_Code, 3, 5)),
+            Unemployment_rate_2015 = Unemployment_rate_2015 / 100,
+            Median_Household_Income_2014) %>%
   write_csv("data/clean/Unemployment.csv")
