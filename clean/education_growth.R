fdab02f7725180753da8344a17b5e9e2f6ab86fe
askilton1 <antonio.skilton@gmail.com>
2016-12-07 15:23:18 -0800

    cleaning file should not perform join

--- clean/education_growth.R
+++ clean/education_growth.R
@@ -1,7 +1,5 @@
 library(tidyverse)
 read_csv("data/clean/Education.csv") %>% 
   group_by(STATEFIPS, COUNTY) %>% 
-  summarise_at(vars(bachelorsOrMore:HSDonly), function(col) last(col) - first(col)) %>% 
-  inner_join(read_csv("data/clean/Pres_Election_Data_2016i.csv")) %>% 
-  select(STATEFIPS, COUNTY, vote_difference, victor, bachelorsOrMore:HSDonly) %>% 
+  summarise_at(vars(bachelorsOrMore:HSDonly), function(col) last(col) - first(col)) %>%
   write_csv("data/clean/Education_growth.csv")
