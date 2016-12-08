015f8d7150c29db3edda421b888df4c8b968d533
askilton1 <antonio.skilton@gmail.com>
2016-12-08 10:20:22 -0800

    final submission

--- SQL/households_view.sql
+++
@@ -1,8 +0,0 @@
-if exists (select * from INFORMATION_SCHEMA.VIEWS where TABLE_NAME = 'census_hhold') drop view census_hhold
-GO 
-
-create view census_hhold as
-	select a.REGION, a.STATEFIP, a.COUNTY, a.METRO, a.HHINCOME, a.AGE, a.RACE, a.white, b.*
-		from census_clean a
-		inner join household_level b
-		on a.SERIAL = b.SERIAL
