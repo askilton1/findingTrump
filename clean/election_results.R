3d5aa94591744bbb7c97b2ef0a88c6bb4cf8587c
askilton1 <antonio.skilton@gmail.com>
2016-12-07 18:01:40 -0800

    added proper FIPS var for tableau dashboard

--- clean/election_results.R
+++ clean/election_results.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-
 read_csv("data/raw/Pres_Election_Data_2016i.csv", 
          na = "-", 
          col_types = cols_only(X1 = col_character(),
@@ -13,12 +12,14 @@ read_csv("data/raw/Pres_Election_Data_2016i.csv",
   slice(-1) %>% 
   rename(county_name = X1, 
          STATEABB = X2,
-         COUNTY = FIPS,
          STATEFIPS = ST) %>% 
   mutate_at(3:4, funs(as.numeric(gsub("%", "", .))/100)) %>% 
   na.omit %>% 
-  transmute(STATEFIPS,
-            COUNTY = as.numeric(substr(COUNTY, 3, 5)),
+  transmute(STATEFIPS, 
+            county_name,
+            STATEABB,
+            FIPS,
+            COUNTY = as.integer(substr(FIPS, 3, 5)),
             vote_difference = Trump - Clinton,
             victor = ifelse(vote_difference > 0, "Trump", "Clinton")) %>% 
   write_csv("data/clean/Pres_Election_Data_2016i.csv", na = "")
